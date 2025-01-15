4109541f75967b360d01389e2ae033bec31eb878
Taha BENMALEK <benmalektaha.inpt@gmail.com>
2025-01-15 18:20:07 +0000

    Created Home page with integration of Stories, Offres, and Banner components

--- Ntriniw/src/pages/Home.jsx
+++ Ntriniw/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../components/Banner.jsx'
 import Stories from '../components/Stories.jsx'
 import Offres from '../components/offres.jsx'
-
+//in this page we have the banner, the stories and the offres
 const Home = () => {
   return (<>
     <Stories/>
